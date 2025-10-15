5a1ade68ba2e7de7e21a44c55afbb20ffa237ff9
krispytank <brianrosh92@gmail.com>
2025-10-15 12:04:35 +0300

    initial

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,4 +11,54 @@ router.get("/", async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message});
     }
-})
+});
+
+// create a product
+router.post("/", async (req, res) => {
+    const { name, description, price, category, inStock } = req.body;
+
+    try {
+        const product = new product({ name, description, price, category, instock});
+        const saved = await product.save();
+        res.status(201).json(saved);
+    } catch (error) {
+        res.status(400).json({ message: error.message});
+    }
+});
+
+
+// update a producct by id
+router.put("/:id", async (req, res) => {
+    try {
+        const product = await product.findByIdAndUpdate(
+            req.params.id,
+            req.boby,
+            { new: true }
+        );
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({ message: error.message});
+    }
+});
+
+// delete a product by id
+router.delete("/:id", async (req, res) => {
+    try {
+        await product.findByIdAndDelete(
+            req.params.id 
+        );
+        res.json({ message: "product deleted" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+// GET /api/products - Get all products
+
+router.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 
 // Import required modules
 const express = require('express');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 const connectDB = require('./config/db');
 const Log = require('./models/log')
+
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware setup
 app.use(bodyParser.json());
@@ -42,76 +50,17 @@ let products = [
 ];
 
 // Routes
+app.use('/products', require("./routes/productRoutes"));
 
 // Root route/endpoint
 app.get('/', (req, res) => {
   res.send('Welcome to the Product API! Go to /api/products to see all products.');
 });
+ 
+
 
 // TODO: Implement the following routes:
-// GET /api/products - Get all products
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-// GET /api/products/:id - Get a specific product
-
-app.get('/api/products/:id', (req, res) =>{
-  const product = products.find(p => p.id === req.params.id);
-  
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  res.json(product);
-});
-
-// POST /api/products - Create a new product
-
-app.post('/api/products', (req, res) => {
-  const { name, description, price, category, inStock } = req.body;
-  if (!name || !description || price == null || !category || inStock == null) {
-    return res.status(400).json({ message: "All fields are required?" });
-  }
-
-  const newProduct = {
-    id: uuidv4(),
-    name,
-    description,
-    price,
-    category,
-    inStock
-  };
-
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
-// PUT /api/products/:id - Update a product
-app.put('/api/products/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const productIndex = products.findIndex(p => p.id === productId);
-  if (productIndex === -1) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  const updateProduct = { ...products[productIndex], ...req.body };
-  products[productIndex] = updateProduct;
-  res.json(updateProduct);
-});
-
-// DELETE /api/products/:id - Delete a product
-
-app.delete('/api/products/:id', (req, res) => {
-  const productIndex = products.findIndex(p => p.id === req.params.id);
-  if (productIndex === -1) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  products.splice(productIndex, 1);
-  res.status(204).send();
-});
-
-// Example route implementation for GET /api/products
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
 
 // TODO: Implement custom middleware for:
 // - Request logging
